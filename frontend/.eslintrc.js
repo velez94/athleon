81d20384f3cb27fba68e32143508e2db40169655
alejandro.velez <labvel@VelPro.>
2025-11-16 11:39:15 -0500

    Add ESLint compatibility for CI environment
    - Simplify lint command to work with both old and new ESLint versions
    - Add .eslintrc.js fallback for older ESLint versions in CI
    - Maintains modern flat config while supporting legacy environments

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: '18.3',
+    },
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off',
+  },
+  ignorePatterns: ['build/', 'node_modules/'],
+}
